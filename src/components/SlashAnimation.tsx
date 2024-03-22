6b34c241bc9c7197fb7cb79682e38789ace87ed3
nandobfer <nandobfer@yahoo.com.br>
2024-03-22 00:57:05 -0300

    feat: added gifs for attacking

--- src/components/SlashAnimation.tsx
+++ src/components/SlashAnimation.tsx
@@ -1,91 +1,35 @@
-import Svg, { Line, Text } from "react-native-svg"
-import Animated, { useAnimatedProps, withSequence, withTiming, Easing, useSharedValue } from "react-native-reanimated"
+import React, { useEffect, useState } from "react"
+import { ImageSourcePropType, View } from "react-native"
 import { uid } from "uid"
-import { useEffect, useState } from "react"
-import schema from "../style/colors.json"
 import { usePlayer } from "../hooks/usePlayer"
+import images from "../images"
+import { Image } from "expo-image"
 
-const AnimatedLine = Animated.createAnimatedComponent(Line)
-// const AnimatedText = Animated.createAnimatedComponent(Text)
-const duration = 1000
-const length = 150
-const maxWidth = 300
-const maxHeight = 400
+const maxWidth = 300 // Maximum X-coordinate for the GIF's position
+const maxHeight = 400 // Maximum Y-coordinate for the GIF's position
 
-const Slash: React.FC<{ damage: number; critical: boolean }> = ({ damage, critical }) => {
-    const startX = useSharedValue(0)
-    const startY = useSharedValue(0)
-    const endX = useSharedValue(0)
-    const endY = useSharedValue(0)
-    const opacity = useSharedValue(1)
-    // const damageOpacity = useSharedValue(1)
-    // const damageY = useSharedValue(0)
-
-    const randomPosition = () => ({
-        x: Math.random() * maxWidth,
-        y: Math.random() * maxHeight,
-    })
-
-    // Define animated properties for the line
-    const animatedSlashProps = useAnimatedProps(() => ({
-        x1: startX.value,
-        y1: startY.value,
-        x2: endX.value,
-        y2: endY.value,
-        strokeOpacity: opacity.value,
-    }))
-
-    // const animatedDamageProps = useAnimatedProps(() => ({
-    //     x: startX.value,
-    //     y: damageY.value,
-    //     fillOpacity: damageOpacity.value,
-    // }))
-
-    const calculateEndPosition = (start: { x: number; y: number }) => {
-        const angle = Math.random() * 2 * Math.PI // Random angle in radians
-        // Ensure a minimum slash length of 100 units
-        return {
-            x: Math.max(0, Math.min(maxWidth, start.x + Math.cos(angle) * length)), // Clamp within [0, maxWidth]
-            y: Math.max(0, Math.min(maxHeight, start.y + Math.sin(angle) * length)), // Clamp within [0, maxHeight]
-        }
-    }
+const SlashGIF: React.FC<{ gif: { source: ImageSourcePropType; width: number; height: number } }> = ({ gif }) => {
+    // Randomly position the GIF
+    const [position, setPosition] = useState({ top: 0, left: 0 })
 
     useEffect(() => {
-        const startPos = randomPosition()
-        const endPos = calculateEndPosition(startPos)
-        startX.value = startPos.x
-        startY.value = startPos.y
-        endX.value = endPos.x
-        endY.value = endPos.y
-        // damageY.value = startY.value - 20
-
-        opacity.value = withSequence(
-            withTiming(1, { duration: 50 }), // Quickly appear
-            withTiming(0, { duration: duration - 50 }) // Then fade out
-        )
-
-        // damageOpacity.value = withSequence(withTiming(1, { duration: 50 }), withTiming(0, { duration: duration - 50 }))
-        // damageY.value = withTiming(startPos.y - 50, { duration: duration }) // Float up effect
+        setPosition({
+            top: Math.random() * maxHeight,
+            left: Math.random() * maxWidth,
+        })
     }, [])
 
     return (
-        <>
-            <AnimatedLine
-                animatedProps={animatedSlashProps}
-                stroke={critical ? schema.colors.stamina : schema.colors.inverseSurface}
-                strokeWidth={"2"}
+        <View style={{ position: "absolute", ...position }}>
+            <Image
+                source={gif.source}
+                style={{
+                    width: gif.width * 2,
+                    height: gif.height * 2,
+                    transform: [{ rotate: `${Math.random() * 360}deg` }, { scaleX: Math.random() > 0.5 ? 1 : -1 }],
+                }}
             />
-            {/* <AnimatedText
-                animatedProps={animatedDamageProps}
-                stroke={schema.colors.inverseSurface}
-                fill={schema.colors.inverseSurface}
-                fontSize="16"
-                y={400}
-                
-            >
-                {damage.toString()}
-            </AnimatedText> */}
-        </>
+        </View>
     )
 }
 
@@ -97,15 +41,22 @@ export const SlashAnimation: React.FC<{}> = ({}) => {
 
     useEffect(() => {
         const timer = setInterval(() => {
-            const { damage, critical } = player.attack(dummy.exp_multiplier)
-            const newSlash = <Slash key={uid(50)} damage={damage} critical={critical} />
+            const { critical } = player.attack(dummy.exp_multiplier)
+
+            const image = critical ? images.crit : images.attack
+
+            const max_index = Object.entries(image).reduce((maximum, [key]) => (Number(key) > maximum ? Number(key) : maximum), 1)
+            const random_index = Math.ceil(Math.random() * max_index)
+            // @ts-ignore
+            const slashGIF = image[random_index]
+            const newSlash = <SlashGIF key={uid(50)} gif={slashGIF} />
             setSlashes((slashes) => [...slashes, newSlash])
 
-            setTimeout(() => setSlashes((slashes) => slashes.filter((item) => item.key != newSlash.key)), duration + 100)
+            setTimeout(() => setSlashes((slashes) => slashes.filter((item) => item.key !== newSlash.key)), slashGIF.duration)
         }, interval)
 
         return () => clearInterval(timer)
-    }, [interval, duration, player.current.attack_speed, dummy])
+    }, [interval, player.current.attack_speed])
 
-    return <Svg style={{ position: "absolute", width: "100%", height: "100%" }}>{slashes}</Svg>
+    return <View style={{ position: "absolute", width: "100%", height: "100%" }}>{slashes}</View>
 }
--- src/images.ts
+++ src/images.ts
@@ -1,7 +1,15 @@
 const images = {
     dummy: {
-        [1]: require('../assets/training/dummy_1.png')
-    }
+        [1]: require("../assets/training/dummy_1.png"),
+    },
+    attack: {
+        [1]: { source: require("../assets/gifs/slash1.gif"), width: 32, height: 54, duration: 600 },
+        [2]: { source: require("../assets/gifs/slash2.gif"), width: 54, height: 27, duration: 400 },
+    },
+    crit: {
+        [1]: { source: require("../assets/gifs/crit1.gif"), width: 45, height: 52, duration: 400 },
+        [2]: { source: require("../assets/gifs/crit2.gif"), width: 41, height: 36, duration: 400 },
+    },
 }
 
 export default images
